f73391299de414e037b54dafd817dfc04323ad00
Ashwini <ashwiniime@gmail.com>
2018-07-27 17:00:48 +1000

    update

--- src/number/rules/examples/Numbers.java
+++ src/number/rules/examples/Numbers.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Numbers {
 	public static Scanner scanner = new Scanner(System.in);
 	public static int n = 5;
-	public static NumberDivisionRules number = new NumberDivisionRules();
+	
 
 	public static void main(String[] args) {
 		for(int i=1; i<10;i++){
